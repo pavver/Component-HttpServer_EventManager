113e57593031a972978c3d251192d8dfc380a2d7
pavver <pavvers1@gmail.com>
2023-09-09 20:04:49 +0300

    Init

---
+++ include/HttpEvents.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "Core.h"
+#include "EventManager.h"
+#include "esp_err.h"
+#include <esp_http_server.h>
+
+__attribute__((noinline)) static esp_err_t post_handler(httpd_req_t *req, uint8_t type, uint8_t subtype, size_t max_content_len = 0, bool isRequest = false)
+{
+  const static char *msg_failed = "Failed to receive";
+
+  if (req->content_len > max_content_len)
+  {
+    httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, msg_failed);
+    httpd_resp_sendstr_chunk(req, NULL);
+    return ESP_FAIL;
+  }
+
+  char *inputData = max_content_len > 0 ? (char *)malloc(sizeof(char) * req->content_len + 1) : nullptr;
+  if (max_content_len > 0 && req->content_len == 0)
+    inputData[0] = '\0';
+
+  if (max_content_len > 0 && req->content_len > 0 && httpd_req_recv(req, inputData, req->content_len + 1) <= 0)
+  {
+    httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, msg_failed);
+    httpd_resp_sendstr_chunk(req, NULL);
+    return ESP_FAIL;
+  }
+  if (isRequest)
+  {
+    EventRequestData *eventReq = (EventRequestData *)eventManager
+                                     ->AddEvent(type, subtype, inputData, isRequest);
+    eventReq->Wait();
+    httpd_resp_sendstr(req, (char *)eventReq->getValue());
+    eventReq->Done();
+  }
+  else
+    eventManager->AddEvent(type, subtype, inputData, isRequest);
+
+  httpd_resp_sendstr_chunk(req, NULL);
+  return ESP_OK;
+}
+
+__attribute__((noinline)) static esp_err_t HandleMethod(const char *method, httpd_req_t *req, esp_err_t(handler)(httpd_req_t *))
+{
+  if (strlen(req->uri) > strlen(method) && cntstr(&req->uri[strlen(req->uri) - strlen(method)], method))
+    return handler(req);
+  return ESP_FAIL;
+}
+
+#define API_METHOD(url, handler)         \
+  err = HandleMethod(url, req, handler); \
+  if (err > ESP_FAIL)                    \
+    return err;
+
+const char *AccessControlAllowOrigin = "Access-Control-Allow-Origin";
+
+static esp_err_t options_handler(httpd_req_t *req)
+{
+  const char *header = "Access-Control-Allow-Methods";
+  httpd_resp_set_hdr(req, AccessControlAllowOrigin, "*");
+  httpd_resp_set_hdr(req, header, "Content-Type");
+  httpd_resp_set_hdr(req, header, "POST, OPTIONS");
+
+  httpd_resp_sendstr_chunk(req, NULL);
+
+  return ESP_OK;
+}
